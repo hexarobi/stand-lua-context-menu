3c98450dadf7eceeb6d370de9bf27c4bcdee8209
Tyler Chamberlain <hexarobi@gmail.com>
2024-05-08 21:04:25 -0500

    Add files via upload

---
+++ lib/ContextMenus/teleport.lua
@@ -0,0 +1,9 @@
+return {
+    name="Teleport",
+    help="Teleport to location",
+    priority=1,
+    --applicable_to={"COORDS"},
+    execute=function(target)
+        ENTITY.SET_ENTITY_COORDS(players.user_ped(), target.pos.x, target.pos.y, target.pos.z)
+    end
+}
